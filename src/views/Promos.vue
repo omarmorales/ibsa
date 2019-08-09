e5afb7b7fbf86d92dc71a038142a7c3ce684b1c3
Omar Saúl Morales Ibarra <omorales@c-230.com>
2019-08-09 18:57:12 -0500

    read promos from firestore

--- src/views/Promos.vue
+++ src/views/Promos.vue
@@ -16,27 +16,61 @@
             <textarea v-model="promo.description" class="form-control" id="description"></textarea>
         </div>
         <button @click="saveData" class="btn btn-primary">Agregar promoción</button>
+        <h2 class="font-weight-bold mt-4">Lista de promociones</h2>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Descripción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="promo in promos" :key="promo.id">
+                    <td>{{ promo.title }}</td>
+                    <td>{{ promo.description }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
     import {fb, db} from '../firebase'
-import { log } from 'util';
+    import { log } from 'util';
     export default {
         data: () => ({
+            promos: [],
             promo: {
                 title: null,
                 description: null,
             }
         }),
         methods: {
+            loadData(){
+                db.collection("promos").get().then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        this.promos.push(doc.data());
+                    });
+                });
+            },
             saveData() {
+                this.$Progress.start();
                 db.collection('promos').add(this.promo)
                 .then((docRef) => {
+                    this.$Progress.finish();
+                    toast.fire({
+                        type: 'success',
+                        title: 'Promoción creada correctamente'
+                    })
                     console.log("Document written with ID: ", docRef.id);
                     this.reset();
                 })
                 .catch((error) => {
+                    this.$Progress.fail();
+                    toast.fire({
+                        type: 'error',
+                        title: 'Error al crear promoción'
+                    })
                     console.log("Error adding document ", error);
                 });
             },
@@ -45,7 +79,7 @@ import { log } from 'util';
             }
         },
         created(){
-
+            this.loadData();
         }
     }
 </script>
