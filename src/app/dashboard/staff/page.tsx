b7c6b8d17576785e85dad5e43fee89855439cd11
Omar Morales <saul090594@gmail.com>
2024-10-17 21:02:30 -0600

    staff view started

--- src/app/dashboard/staff/page.tsx
+++ src/app/dashboard/staff/page.tsx
@@ -1,3 +1,4 @@
+import prisma from "@/lib/db";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import {
   Breadcrumb,
@@ -8,24 +9,58 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
-const Staff: React.FC = () => {
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default async function Staff() {
+  // const staffMembers = await prisma.staff.findMany();
+
+  const staffMembers = await prisma.staff.findMany();
+
   return (
     <MaxWidthWrapper className="pb-24 pt-10 sm:pb-32 lg:pt-10 xl:pt-10 lg:pb-52">
       {/* Breadcrumb starts */}
       <Breadcrumb className="mb-8">
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Personal</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-        {/* Breadcrumb ends */}
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Personal</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* Breadcrumb ends */}
+
+      {/* Staff members list starts */}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Rol</TableHead>
+            <TableHead>Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {staffMembers.map((staffMember) => (
+            <TableRow key={staffMember.id} >
+              <TableCell>1</TableCell>
+              <TableCell>{ staffMember.name }</TableCell>
+              <TableCell>{ staffMember.role }</TableCell>
+              <TableCell>Acciones</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {/* Staff members list ends */}
     </MaxWidthWrapper>
   );
-};
-
-export default Staff;
+}
